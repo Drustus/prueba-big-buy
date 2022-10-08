2228c96bd2586912e6cb65fe5bb40b030894129e
Drustus <cararlai@gmail.com>
2022-10-08 12:40:18 +0200

    Añadido icono

--- src/pages/movements/Title/index.tsx
+++ src/pages/movements/Title/index.tsx
@@ -1,7 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import "./styles.scss";
 
 const Title = () => {
-  return <div className="title">Movimientos</div>;
+  return (
+    <div className="title">
+      <FontAwesomeIcon className="title-icon" icon={faArrowRightArrowLeft} />
+      Movimientos
+    </div>
+  );
 };
 
 export default Title;
